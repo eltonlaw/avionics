d38324c11a609c0a9f25a0d892ba674683a89d3a
Elton Law <eltonlaw296@gmail.com>
2023-09-01 23:16:49 -0400

    fix: add missing decl

--- avlib/i2c.h
+++ avlib/i2c.h
@@ -9,5 +9,6 @@ uint8_t i2c_read_ack(void);
 uint8_t i2c_read_nack(void);
 void i2c_write(uint8_t data);
 void i2c_write_sla_w(uint8_t data);
+void i2c_write_sla_r(uint8_t data);
 void i2c_write_data(uint8_t data);
 uint8_t i2c_status(void);
