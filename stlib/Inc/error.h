1df791cb689bf20a58ebbf45dea698d0e020f3ca
Elton Law <eltonlaw296@gmail.com>
2023-10-22 13:45:23 -0400

    make error_t a superset of HAL_StatusTypeDef

--- stlib/Inc/error.h
+++ stlib/Inc/error.h
@@ -1,7 +1,12 @@
 #pragma once
+#include "stm32g0xx.h"
 
 enum error_t {
-    E_OK = 0,
+    E_HAL_OK = HAL_OK,
+    E_HAL_ERROR = HAL_ERROR,
+    E_HAL_BUSY = HAL_BUSY,
+    E_HAL_TIMEOUT = HAL_TIMEOUT,
+    E_OK,
     E_I2C_WRONG_DEVICE,
     // Catch all, discouraged
     E_UNKNOWN,
--- stlib/Inc/mpu6050.h
+++ stlib/Inc/mpu6050.h
@@ -1,16 +1,17 @@
+#pragma once
+
 #include <math.h>
 #include <stdint.h>
 #include "stm32g0xx.h"
 #include "error.h"
 
-#define MPU6050_WHO_AM_I_REG 0x75
-#define MPU6050_PWR_MGMT_1_REG 0x6B
+#define MPU6050_ADDR (0x68 << 1)
+
+#define MPU6050_GYRO_CONFIG_REG 0x1B
 #define MPU6050_ACCEL_CONFIG_REG 0x1C
 #define MPU6050_ACCEL_XOUT_H_REG 0x3B
-#define MPU6050_TEMP_OUT_H_REG 0x41
-#define MPU6050_GYRO_CONFIG_REG 0x1B
-#define MPU6050_GYRO_XOUT_H_REG 0x43
-#define MPU6050_ADDR (0x68 << 1)
+#define MPU6050_PWR_MGMT_1_REG 0x6B
+#define MPU6050_WHO_AM_I_REG 0x75
 
 #define MPU6050_PWR_MGMT_OFF 0
 
