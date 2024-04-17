0184b1a42a37faae9aa114f85c1f8a0d32b9694a
Elton Law <eltonlaw296@gmail.com>
2024-04-16 23:34:31 -0400

    docs: add sam m10q docstrings + stubs

--- stlib/Inc/sam_m10q.h
+++ stlib/Inc/sam_m10q.h
@@ -3,6 +3,34 @@
 #include "stm32g0xx.h"
 #include "error.h"
 
+#define SAM_M10Q_MESSAGE_HEADER 0xb562
+#define SAM_M10Q_CLASS_ID_UBX_CFG_VALGET 0x068b
+#define SAM_M10Q_CLASS_ID_UBX_MON_VER 0x0a04
+
+#define SAM_M10Q_ADDR (0x42 << 1)
+#define SAM_M10Q_NUM_BYTES_TO_READ_H 0xFD
+#define SAM_M10Q_NUM_BYTES_TO_READ_L 0xFE
+#define SAM_M10Q_VALUE 0xFF
+
+/* ublox datatypes from interface description
+ *
+ * L: single-bit boolean (true=1, false=0) stored as U1
+ * U1/2/4/8: unsigned little-endian integer 8/16/32/64-bit
+ * X1/2/4/8: unsigned little-endian integers of 8/16/32/64-bit
+ * */
+#define SAM_M10Q_CFG_KEY_I2C_ADDRESS 0x20510001 // U1
+#define SAM_M10Q_CFG_KEY_I2C_ENABLED 0x10510003 // L
+#define SAM_M10Q_CFG_KEY_INFMSG_UBX_I2C 0x20920001 // X1
+#define SAM_M10Q_CFG_KEY_INFMSG_UBX_UART1 0x20920002 // X1
+#define SAM_M10Q_CFG_KEY_INFMSG_NMEA_I2C 0x20920006 // X1
+#define SAM_M10Q_CFG_KEY_INFMSG_NMEA_UART1 0x20920007 // X1
+
+#define SAM_M10Q_CFG_VAL_INFMSG_ERROR 0x01
+#define SAM_M10Q_CFG_VAL_INFMSG_WARNING 0x02
+#define SAM_M10Q_CFG_VAL_INFMSG_NOTICE 0x04
+#define SAM_M10Q_CFG_VAL_INFMSG_TEST 0x08
+#define SAM_M10Q_CFG_VAL_INFMSG_DEBUG 0x10
+
 typedef struct {
     I2C_HandleTypeDef* i2cx;
 	UART_HandleTypeDef* uartx;
@@ -14,3 +42,6 @@ typedef struct {
 
 error_t sam_m10q_init(sam_m10q_cfg_t* cfg);
 error_t sam_m10q_read(sam_m10q_cfg_t* cfg, sam_m10q_data_t *data);
+error_t ubx_msg_update_ck(uint8_t len, uint8_t* frame);
+error_t ubx_msg_new(uint8_t len, uint8_t* payload);
+error_t ubx_mon_ver(void);
--- stlib/Src/sam_m10q.c
+++ stlib/Src/sam_m10q.c
@@ -1,10 +1,70 @@
-/* sam_m10q.c
+/*
+ * sam_m10q.c
  *
+ * - RAM receiver configuration is configured on startup each time, power-down and reset clears it.
+ * - Battery-backed RAM (BBR) is also used to store config, when applying runtime configs, it should be applyed to RAM and to BBR
+ * - "Neither handshaking signals nor hardware flow control signals are supported" for UART, so use a bus that doesn't have CTS and RTS.
  */
 #include "log.h"
 #include "sam_m10q.h"
 
+/*
+ * - Poll request type
+ * - Message returns a UBX-ACK-NACK
+ * - You can use wildcard by specifying the group specifier but put 0xffff in bits 0-15
+ * - message is limited to a max of 64 key ids
+ * - byte[0] (U1): version
+ * - byte[1] (U1): layer
+ * - byte[2:3] (U2): position
+ * - byte[4:4+n*4] (U4): repeated key ids
+ * */
+error_t ubx_valget(uint32_t key_id) {
+    // message is limited to a max of 64 key ids
+    // uint32_t key_ids[64];
+    return E_OK;
+}
+
+/* The checksum is calculated over the message, starting and including the
+ * class ﬁeld up until, but excluding, the checksum ﬁelds */
+error_t ubx_msg_update_ck(uint8_t len, uint8_t* frame) {
+    uint8_t ck_a_i = len - 1;
+    uint8_t ck_b_i = len - 2;
+    // start at 2 to skip device message header, end when CK_A reached
+    for (uint8_t i=2; i < ck_b_i; i++) {
+        frame[ck_a_i] = frame[i] + frame[ck_a_i];
+        frame[ck_b_i] = frame[ck_b_i] + frame[ck_a_i];
+    }
+    return E_OK;
+}
+
+error_t ubx_msg_new(uint8_t len, uint8_t* payload) {
+    return E_OK;
+}
+
+/*
+ * Header: 0xb6 0x62
+ * Class: 0x0a
+ * Id: 0x04
+ * Length (bytes): 0
+ * Payload: This message has no payload
+ * Checksum: CK_A CK_B
+ * */
+error_t ubx_mon_ver(void) {
+    error_t err;
+    uint8_t frame[] = {0xb5, 0x62, 0x0a, 0x04, 0x00, 0x00, 0x00, 0x00};
+    err = ubx_msg_update_ck(8, frame);
+    return err;
+}
+
 error_t sam_m10q_init(sam_m10q_cfg_t* cfg) {
+    // FIXME: VALSET info message configuration
+    // FIXME: VALGET info message
+    // FIXME: VALSET enable i2c
+    // FIXME: VALSET set i2c address
+    // FIXME: Configure NMEA output configuration for multi-gnss
+    // FIXME: Set a higher update rate
+    // FIXME: Check if signal lock found
+    // FIXME: Configure RTK networks
     log_info("sam_m10q_init");
     return E_OK;
 }
