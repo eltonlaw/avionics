190c47d12aa57f4264129761f78b66394e9fc9b2
Elton Law <eltonlaw296@gmail.com>
2024-04-17 23:57:15 -0400

    chore: sim - modularize main.cpp

---
+++ physics_sim/include/render.hpp
@@ -0,0 +1,18 @@
+// System Headers
+#define GLEW_STATIC
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// Standard Headers
+#include <cstdio>
+#include <cstdlib>
+
+namespace sim {
+
+const int mWidth = 800;
+const int mHeight = 600;
+
+int render();
+
+} // namespace sim
--- physics_sim/src/main.cpp
+++ physics_sim/src/main.cpp
@@ -1,71 +1,10 @@
-// System Headers
-#define GLEW_STATIC
-#include <stdio.h>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include "render.hpp"
+#include "data.hpp"
 
-// Standard Headers
-#include <cstdio>
-#include <cstdlib>
-
-const int mWidth = 800;
-const int mHeight = 600;
-
-static void error_callback(int error, const char* description) {
-    fprintf(stderr, "Error: %s\n", description);
-}
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
+using namespace sim;
 
 int main(int argc, char * argv[]) {
-    glfwSetErrorCallback(error_callback);
-
-    // Load GLFW and Create a Window
-    if (glfwInit() == false) {
-        fprintf(stderr, "Failed to Initialize GLFW");
-        exit(EXIT_FAILURE);
-    }
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    GLFWwindow* window = glfwCreateWindow(mWidth, mHeight, "Simulator", nullptr, nullptr);
-
-    // Check for Valid Context
-    if (window == nullptr) {
-        glfwTerminate();
-        fprintf(stderr, "Failed to Create OpenGL Context");
-        exit(EXIT_FAILURE);
-    }
-
-    // Create Context and Load OpenGL Functions
-    glfwMakeContextCurrent(window);
-    glewExperimental = GL_TRUE;
-    glewInit();
-    glfwSwapInterval(1);
-    glfwSetKeyCallback(window, key_callback);
-    fprintf(stdout, "OpenGL %s\n", glGetString(GL_VERSION));
-
-    // Rendering Loop
-    while (!glfwWindowShouldClose(window)) {
-        // Background Fill Color
-        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Swap front and back buffers
-        glfwSwapBuffers(window);
-        // Check if any events are triggered and update windows state, calling
-        // any corresponding callbacks and returns immediately
-        glfwPollEvents();
-
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(window, GL_TRUE);
-    }
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    exit(EXIT_SUCCESS);
+    (void) argc;
+    (void) argv;
+    return sim::render();
 }
