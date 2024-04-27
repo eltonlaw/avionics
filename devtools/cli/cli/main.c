f3445257d121a7ab4df20eb4debdde548f07329a
Elton Law <eltonlaw296@gmail.com>
2024-04-27 01:39:11 -0400

    feat: pass ret codes back from cli cmds

--- devtools/cli/cli/main.c
+++ devtools/cli/cli/main.c
@@ -3,41 +3,46 @@
 #include <string.h>
 #include <unistd.h>
 
-typedef void (*command_fn)(int, char**);
+typedef int (*command_fn)(int, char**);
 
-void help(int argc, char **argv);
+int help(int argc, char **argv);
 
 typedef struct {
     char *type;
     command_fn fn;
-	char *description;
+    char *description;
 } command;
 
-void serial(int argc, char **argv) {
-    system("ipython -i -c \"%load devtools/serial_interactive.py\"");
+int serial(int argc, char **argv) {
+    return system("ipython -i -c \"%load devtools/serial_interactive.py\"");
 }
 
-void avr_gcc(int argc, char **argv) {
-    system("avr-gcc -print-search-dirs");
+int avr_gcc(int argc, char **argv) {
+    return system("avr-gcc -print-search-dirs");
 }
 
-void fc1(int argc, char **argv) {
-    system("cd fc1 && make clean && make flash");
+int fc1(int argc, char **argv) {
+    return system("cd fc1 && make clean && make flash");
 }
 
-void sensor_calibrate(int argc, char **argv) {
-    system("cd sensor_calibrate && idf.py build");
+int sensor_calibrate(int argc, char **argv) {
+    return system("cd sensor_calibrate && idf.py build");
 }
 
-void sim(int argc, char **argv) {
-    system("cd ./physics_sim/build && cmake ../ && make && ./sim");
+int sim(int argc, char **argv) {
+    return system("cd ./physics_sim/build && cmake ../ && make && ./sim");
 }
 
-void gen_compile_commands(int argc, char **argv) {
-    system("cmake -Bfc3/build fc3");
-    if (access("compile_commands.json", R_OK) != 0) {
-        system("ln -s fc3/build/compile_commands.json .");
-    }
+int gen_compile_commands(int argc, char **argv) {
+    int ret;
+    if (0 != (ret = system("cmake -Bfc3/build fc3")))
+        return ret;
+    /* If compile_commands.json already exists, skip soft linking it */
+    if (0 == (ret = access("compile_commands.json", R_OK)))
+        return ret;
+    if (0 != (ret = system("ln -s fc3/build/compile_commands.json .")))
+        return ret;
+    return ret;
 }
 
 command commands[] = {
@@ -51,25 +56,25 @@ command commands[] = {
     {NULL, NULL, NULL}
 };
 
-void help(int argc, char *argv[]) {
+int help(int argc, char *argv[]) {
     printf("Available commands:\n");
     for (int i = 0; commands[i].type != NULL; i++) {
         printf("  %s: %s\n", commands[i].type, commands[i].description);
     }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
     if (argc < 2)
-		goto invalid_input;
+        goto invalid_input;
 
     for (int i = 0; commands[i].type != NULL; i++) {
         if (strcmp(argv[1], commands[i].type) == 0) {
-            commands[i].fn(argc, argv);
-            return 0;
+            return commands[i].fn(argc, argv);
         }
     }
 invalid_input:
-	printf("Usage: %s <command>\n", argv[0]);
-	help(argc, argv);
-	return 1;
+    printf("Usage: %s <command>\n", argv[0]);
+    help(argc, argv);
+    return 1;
 }
